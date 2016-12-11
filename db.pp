d0445ead8d990ccd5d103b6978a527aa9bdaa687
Alex <aleksandr.pantsesnyi@myy.haaga-helia.fi>
2016-12-11 14:55:36 +0200

    Create db.pp

---
+++ db.pp
@@ -0,0 +1,43 @@
+class wordpress::db {
+  
+    class { '::mysql::server':
+    
+      # Set the root password
+        root_password => $wordpress::conf::root_password,
+
+      # Create the database
+        databases => {
+          "${wordpress::conf::db_name}" => {
+              ensure => 'persent',
+              charset => 'utf8'
+          }
+       },
+        
+       # Create the user
+       users => {
+          "${wordpress::conf::db_user_host}" => {
+              ensure => 'persent',
+              password_hash => mysql_password("${wordpress::conf::db_user_password}")
+           }
+        },
+        
+        # Grant privileges to the user
+        grants => {
+            "${wordpress::conf:db_user_host_db}" => {
+                ensure => 'persent',
+                option => ['GRANT'],
+                privileges =>['ALL'],
+                table => "${wordpress::conf::db_name}.*",
+                user => "${wordpress::conf::db_user_host}",
+               }
+             },
+           }
+           
+           
+    # Install MySQL client and all bindings
+    class { '::mysql::client':
+        require => Class['::mysql::server'],
+        bindings_enable => true
+        
+      }
+    }
