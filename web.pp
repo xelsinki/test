d79b24b0125606e2570b22802933206ce893772f
Alex <aleksandr.pantsesnyi@myy.haaga-helia.fi>
2016-12-14 06:07:13 +0000

    Update web.pp

--- web.pp
+++ web.pp
@@ -1,15 +1,22 @@
 class wordpress::web {
 
-  # Install Apache
-  # I use prefork as the MPM (Multi-Processing Module) 
-  # to maximize compatibility with other libraries.
-    
-    class {'apache':
-        mpm_module => 'prefork'
-   }
-   
-   # Add support for PHP
-    
-    class {'::apache::mod::php':
-    }
- }
+  class { 'apache':
+    mpm_module => 'prefork',
+    default_vhost => false,
+  }
+
+  apache::vhost { 'wordpress':
+    port     => '80',
+    docroot  => '/var/www/wordpress',
+  }
+
+  include apache::mod::php
+
+  package {
+  "php5-mysql":
+    ensure  => present;
+  }
+
+}
+
+
